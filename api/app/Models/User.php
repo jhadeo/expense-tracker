a5b978a049d899f580ccc359d80c0cda6a62dbc2
jhadeo <105715821+jhadeo@users.noreply.github.com>
2026-07-29 09:37:53 +0800

    feat: modify user migration and model

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['first_name','last_name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,5 +28,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected $appends = ['full_name'];
+
+    public function getFullNameAttribute() : string {
+        return trim("{$this->first_name} {$this->last_name}");
     }
 }
